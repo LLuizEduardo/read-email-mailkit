9d2d1fb0cab9e6782e20096ec94379093322e88d
Filipe Ribeiro <68390743+FilipeRibeiroDev@users.noreply.github.com>
2022-11-19 19:04:12 -0300

    First Commit

---
+++ ReadEmail/ENV.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadEmail
+{
+    public static class ENV
+    {
+        public static string PASSWORD = "Se inscreva no canal Filipe Brito DEV https://bit.ly/3JMK2ox";
+    }
+}
---
+++ ReadEmail/Program.cs
@@ -0,0 +1,7 @@
+﻿using ReadEmail;
+
+var email = new Email(iMAP_HOST: "outlook.office365.com", iMAP_USER: "ribeiro12369@hotmail.com", iMAP_PASSWORD: ENV.PASSWORD);
+await email.Connect();
+var mesages = email.GetMessages();
+
+Console.ReadLine();
